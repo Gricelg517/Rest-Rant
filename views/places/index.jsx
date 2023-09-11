77ba7502b3057fea824c38b68083a030df0d9b41
Gricelg517 <135929737+Gricelg517@users.noreply.github.com>
2023-09-11 16:33:39 -0500

    adding styles to REST-Rant

--- views/places/index.jsx
+++ views/places/index.jsx
@@ -5,7 +5,12 @@ function index (data) {
     let placesFormatted = data.places.map((place) => {
       return (
         <div>
-          <h2>{place.name}</h2>
+          <h2>
+          <a href="/places">
+                <button className="btn-primary">Places Page</button>
+          </a>
+
+          </h2>
           <img src={place.pic} alt={place.name}/>
         </div>
       )
