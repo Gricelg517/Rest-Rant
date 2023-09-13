292ced2d500c4d7b77166b22cdc2f40aef69d926
Gricelg517 <135929737+Gricelg517@users.noreply.github.com>
2023-09-13 17:20:40 -0500

    Navbar with style

--- controllers/Places.js
+++ controllers/Places.js
@@ -1,4 +1,5 @@
 const router = require('express').Router()
+const places = require('../models/places.js')
 
 // GET /places
 router.get('/', (req, res) => {
@@ -20,10 +21,21 @@ router.get('/', (req, res) => {
 router.get('/new', (req, res) => {
         res.render('places/new')
       })
-router.post('/', (req, res) => {
-    console.log(req.body)
+      router.post('/', (req, res) => {
+        console.log(req.body)
+        if (!req.body.pic) {
+          // Default image if one is not provided
+          req.body.pic = 'http://placekitten.com/400/400'
+        }
+        if (!req.body.city) {
+          req.body.city = 'Anytown'
+        }
+        if (!req.body.state) {
+          req.body.state = 'USA'
+        }
         res.send('POST /places')
       })
+      
       
       
   })
