680543d2d495d52ddd3c06c6fb2966df4904e92b
Gricelg517 <135929737+Gricelg517@users.noreply.github.com>
2023-09-16 08:11:37 -0500

    Added the edit form

--- controllers/Places.js
+++ controllers/Places.js
@@ -60,7 +60,19 @@ router.get('/new', (req, res) => {
           res.redirect('/places')
           res.send('STUB DELETE places/:id')
         }
-      })
+        router.get('/:id/edit', (req, res) => {
+          let id = Number(req.params.id)
+          if (isNaN(id)) {
+              res.render('error404')
+          }
+          else if (!places[id]) {
+              res.render('error404')
+          }
+          else {
+            res.render('places/edit', { place: places[id] })
+          }
+        })
+        
       
         
       
