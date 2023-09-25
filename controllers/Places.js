2631d18057d931521767f50f77e5f9fbcfef1e93
Gricelg517 <135929737+Gricelg517@users.noreply.github.com>
2023-09-25 17:15:18 -0500

    create a comment

--- controllers/Places.js
+++ controllers/Places.js
@@ -43,20 +43,29 @@ router.get('/new', (req, res) => {
 })
 
 // SHOW ROUTE
-  router.get('/:id', (req, res) => {
-    db.Place.findById(req.params.id)
-    .populate('comments')
+  router.get('/:id/comment', (req, res) => {
+    console.log(req.body)
+     db.Place.findById(req.params.id)
+     .populate('comments')
       .then(place => {
-        console.log(place.commments)
-        res.render('places/show', { place })
+        db.Comment.create(req.body)
+        .then(comment =>{
+          // Todo: Save Comment id to place
+          place.comments.push(comment.id)
+            place.save()
+            .then(() => {
+                res.redirect(`/places/${req.params.id}`)
+            })
+          })
+          .catch(err => {
+              res.render('error404')
+          })
       })
       .catch(err => {
-        console.log('err', err)
-        res.render('error404')
+          res.render('error404')
       })
   })
-
-
+  
 router.put('/:id', (req, res) => {
   res.send('PUT /places/:id stub')
 })
